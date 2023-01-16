451bf77cafc58618d618e53d24b847dd3f63cc8c
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-01-16 15:16:42 +0100

    add: Api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script>
+import { store } from './store.js';
 import appHeader from './components/appHeader.vue';
 import appMain from './components/appMain.vue';
 import appFooter from './components/appFooter.vue';
+import axios from 'axios';
 
 export default {
   name: 'AppVue',
@@ -9,8 +11,32 @@ export default {
     appHeader,
     appMain,
     appFooter,
+  },
+  data() {
+      return {
+      store,
+      key:'e0881c78a00708611e8a0dd128480c52',
+      url: 'https://api.themoviedb.org/3/movie/550?api_key=e0881c78a00708611e8a0dd128480c52',
+    }
+  },
+  methods:{
+    getMovie(){
+      axios.get(this.url,{
+        params:{
 
+        }
+
+      })
+      .then((response) =>{
+        console.log(response.data)
+        this.store.movie = response.data.data
+      })
+    } 
+  },
+  created(){
+    this.getMovie()
   }
+ 
 }
 
 </script>
--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,12 +1,21 @@
 <script>
+import { store } from '../store'
 export default{
     name: 'appHeader',
+    data(){
+      return {
+         store,
+         film: '',
+       }
+   },
 }
 </script>
 
 <template>
     <div>
-
+        <label for=""></label>
+        <input type="text" placeholder="inserisci il tuo film" v-model="film">
+        <button>Clicca</button>
     </div>
 </template>
 
